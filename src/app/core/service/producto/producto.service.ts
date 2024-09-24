7f757890f37cf45db1dc4d035008dbe2aba6ca59
BrandonDev <brandondev1996@gmail.com>
2024-09-24 12:38:10 -0300

    refactor in url path

--- src/app/core/service/producto/producto.service.ts
+++ src/app/core/service/producto/producto.service.ts
@@ -9,7 +9,7 @@ import { Producto } from '../../models/producto';
 export class ProductoService {
 
   constructor(private http: HttpClient) {}
-  url = "https://http://localhost:8080/producto/api-gestor-de-productos/producto"
+  url = "https://cvappbackend.onrender.com/api-gestor-de-productos/producto"
 
   getProductos(): Observable<Producto[]> {
     return this.http.get<Producto[]>(this.url);
